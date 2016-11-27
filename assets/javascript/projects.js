1b260d9fbab8fbe34d182c4439c88cfb2a5ab722
Esther Leytush <eleytush@gmail.com>
2016-11-27 01:15:44 -0500

    build basis for tabbed projects

--- assets/javascript/projects.js
+++ assets/javascript/projects.js
@@ -1,41 +1,30 @@
-(function($){
-	$(document).ready(function(){
-		$(document).on('click','.tag-filter',function(){
-			var $this = $(this);
-			
-			$('span.tag-filter').removeClass("active");
-			$this.addClass("active");
+function toggleProjects(selected) {
+	var projects = $(".projects-wrapper").children();
 
+	$.each(projects, function(element) {
+		var current = $(projects[element])
 
-			$('.tag-group').children().each(function(){
-				if( $(this).data('tag') ==  $this.data('tag')){
-					$(this).addClass("active");
-				}
-			});
+		if ((current.hasClass(selected))) {
+			$(current).removeClass("hidden");
+			$(current).addClass("visible");
+		} else {
+			$(current).removeClass("visible");
+			$(current).addClass("hidden");
+		}
+	});
+}
 
-			if( $(this).hasClass('all'))
-				$('.project-item').showAll();
-			else
-				$('.project-item').filterTags( $(this).data('tag') );
-		});
+function toggleTabs(selected) {
+	$("li.active").removeClass("active");
+	$(selected).addClass("active");
+}
+
+$(document).ready(function(){
+
+	$(".tab").on("click", function(e) {
+		var selected = $(this).text()
+		toggleTabs($(this));
+		toggleProjects(selected.toLowerCase());
 	});
 
-	$.fn.extend({
-	  filterTags: function(tagName) {
-	  	this.removeClass('not-show');
-	    return this.each(function() {
-	    	var itemTagArray = JSON.parse( $(this).attr('data-tags') );
-			if($.inArray(tagName, itemTagArray) === -1){
-				$(this).addClass('not-show');
-			}
-	    });
-	  },
-	  showAll: function(){
-	  	return this.each(function() {
-			if($(this).hasClass('not-show')){
-				$(this).removeClass('not-show');
-			}
-	    });
-	  }
-	});
-})(jQuery)
+});
