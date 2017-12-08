872470b2c13eafa32eea2ab940581d76299acb3f
Esther Leytush <eleytush@gmail.com>
2017-12-08 16:53:28 -0500

    create initial dark layout

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,18 +3,15 @@ module.exports = function(grunt) {
   grunt.initConfig({
 
     // -------------------------------------------------------------------------------------
-    // This task resizes all NEWLY ADDED images placed in `assets/development/images/`
-    // and places them into `assets/production/images/`.
+    // `grunt resize` : This task resizes all new images placed in `blog/assets/development/images/`
+    // and places them into `blog/assets/production/images/`.
     //
     // Relies on 'grunt-responsive-images': https://github.com/andismith/grunt-responsive-images
-    //
-    // To run this task manually: cd into /blog/, then `grunt resize`
     responsive_images: {
-      myTask: {
+      convertLarge: {
         options: {
-          sizes: [
-          {
-            width: 800,
+          sizes: [{
+            width: 1200
           }]
         },
         files: [{
@@ -23,53 +20,38 @@ module.exports = function(grunt) {
           cwd: 'assets/original_images',
           custom_dest: 'assets/images'
         }]
-      }
+      },
     },
 
     // -------------------------------------------------------------------------------------
-    // This task compiles the main Sass file, `assets/development/stylesheets/main.scss`,
-    // into `assets/development/main.css`
+    // This task compiles the main Sass file, `assets/stylesheets/main.scss`,
+    // into `assets/stylesheets/main.css`
     //
     // Relies on 'grunt-contrib-sass': https://github.com/gruntjs/grunt-contrib-sass
     //
-    // To run this task manually: cd into /blog/, then `grunt sass`
+    // To run this task manually: `grunt sass`
     sass: {
       dist: {
         options: {
           style: 'expanded'
         },
         files: {
-          'assets/stylesheets/main-compiled.css': 'assets/stylesheets/main.scss'
+          'assets/stylesheets/main.css': 'assets/stylesheets/main.scss'
         }
       }
     },
 
     // -------------------------------------------------------------------------------------
-    // This task watches the main Sass file, `assets/development/stylesheets/main.scss`,
+    // This task watches the main Sass file, `assets/stylesheets/main.scss`,
     // and runs the 'sass' task (above).
     //
     // Relies on 'grunt-contrib-watch': https://github.com/gruntjs/grunt-contrib-watch
     //
-    // To run this task manually: cd into /blog/, then `grunt watch`
+    // To run this task manually: `grunt watch`
     watch: {
       css: {
-        files: 'assets/stylesheets/*.scss',
-        tasks: ['sass', 'cssmin']
-      }
-    },
-
-    // -------------------------------------------------------------------------------------
-    // This task compresses the main CSS file, `assets/development/main.css`,
-    // and places compressed file into `assets/production/main.css`
-    //
-    // Relies on 'grunt-contrib-cssmin': https://github.com/gruntjs/grunt-contrib-cssmin
-    //
-    // To run this task manually: cd into /blog/, then `grunt cssmin`
-    cssmin: {
-      target: {
-        files: {
-          'assets/stylesheets/main.min.css': 'assets/stylesheets/main-compiled.css'
-        }
+        files: "**/*.scss",
+        tasks: ['sass']
       }
     },
 
@@ -80,13 +62,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-responsive-images');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-
-  grunt.registerTask('resize', ['responsive_images']);
 
   // -------------------------------------------------------------------------------------
+  grunt.registerTask('resize', ['responsive_images']);
 
-  // 'default' preps all assets for deployment.
+  // 'default' resizes and moves images into /images folder and compiles Sass to CSS.
   // To run manually, run `grunt default`
-  grunt.registerTask('default', ["resize", 'sass', 'cssmin']);
+  grunt.registerTask('default', ["resize", 'sass']);
 };
