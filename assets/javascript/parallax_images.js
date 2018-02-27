badfec06a897210a4f24d1557350cfed6c47d26d
Esther Leytush <eleytush@gmail.com>
2018-02-26 19:49:41 -0500

    fix unsplash author referral link

--- assets/javascript/parallax_images.js
+++ assets/javascript/parallax_images.js
@@ -14,43 +14,43 @@ function fallbackImages(){
   var availableImages = [
     { src: base + "mountain.jpg",
       user: "Samuel Scrimshaw",
-      photoLink: "https://unsplash.com/photos/2oFdVd00xOg" },
+      userLink: "https://unsplash.com/photos/2oFdVd00xOg" },
 
     { src: base + "north.jpg",
       user: "Martin Jernberg",
-      photoLink: "https://unsplash.com/photos/UdURxHDhrgY" },
+      userLink: "https://unsplash.com/photos/UdURxHDhrgY" },
 
     { src: base + "phone-flower.jpg",
       user: "Tyler Mullins",
-      photoLink: "https://unsplash.com/photos/H-lDpIVIlBM" },
+      userLink: "https://unsplash.com/photos/H-lDpIVIlBM" },
 
     { src: base + "stars.jpg",
       user: "Nathan Anderson",
-      photoLink: "https://unsplash.com/photos/L-7cP4p5hik" },
+      userLink: "https://unsplash.com/photos/L-7cP4p5hik" },
 
     { src: base + "lady.jpg",
       user: "Miles Tan",
-      photoLink: "https://unsplash.com/photos/RFgO9B_OR4g" },
+      userLink: "https://unsplash.com/photos/RFgO9B_OR4g" },
 
     { src: base + "wolf.jpg",
       user: "Marek Szturk",
-      photoLink: "https://unsplash.com/photos/BiSFSSQtAmg" },
+      userLink: "https://unsplash.com/photos/BiSFSSQtAmg" },
 
     { src: base + "stones.jpg",
       user: "Danny Postma",
-      photoLink: "https://unsplash.com/photos/XqtJY5gTo5k" },
+      userLink: "https://unsplash.com/photos/XqtJY5gTo5k" },
 
     { src: base + "coupola.jpg",
       user: "Skiathos Greece",
-      photoLink: "https://unsplash.com/photos/IRaJVADIVAE" },
+      userLink: "https://unsplash.com/photos/IRaJVADIVAE" },
 
     { src: base + "gardens.jpg",
       user: "贝莉儿 NG",
-      photoLink: "https://unsplash.com/photos/dqXiw7nCb9Q" },
+      userLink: "https://unsplash.com/photos/dqXiw7nCb9Q" },
 
     { src: base + "coffee.jpg",
       user: "AJ Garcia",
-      photoLink: "https://unsplash.com/photos/i_6Y2V81ceA" }
+      userLink: "https://unsplash.com/photos/i_6Y2V81ceA" }
   ];
 
   shuffle(availableImages);
@@ -71,7 +71,7 @@ $(document).ready(function(){
           var photo = {
             src: item.urls.regular,
             user: item.user.name,
-            photoLink: item.links.html
+            userLink: item.user.links.html
           }
 
           photos.push(photo);
@@ -86,7 +86,7 @@ $(document).ready(function(){
       imageSRC = image.src,
       user = image.user,
       referralText = "?utm_source=personal_site&utm_medium=referral&utm_content=creditCopyText",
-      photoLink = image.photoLink + referralText,
+      userLink = image.userLink + referralText,
       unsplash = "https://unsplash.com/" + referralText;
 
       $(item).parallax({
@@ -98,7 +98,7 @@ $(document).ready(function(){
       $(item).append(attributionElement);
 
       function clearCover(){
-        var attribution = "Photo by <a href='" + photoLink + "' target='_blank'>" + user + "</a> on <a href='" + unsplash + "' target='_blank'>Unsplash</a>";
+        var attribution = "Photo by <a href='" + userLink + "' target='_blank'>" + user + "</a> on <a href='" + unsplash + "' target='_blank'>Unsplash</a>";
         $(item).find(".attribution").append(attribution);
         $(item).addClass("parallax-loaded");
       };
@@ -111,7 +111,7 @@ $(document).ready(function(){
     appendPhotos();
   });
 
-  // Handler for when Unsplash API is down 
+  // Handler for when Unsplash API is down
   window.addEventListener("unhandledrejection", function(e) {
     e.preventDefault();
     var reason = e.detail.reason;
